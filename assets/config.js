75dc3dd5c2e50b0bf37e7c4f5968416f133ae04a
mathislef34 <116552541+mathislef34@users.noreply.github.com>
2025-09-23 23:39:53 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,7 @@
 // ==== Configuration GitHub (à adapter) ====
 window.APP_CONFIG = {
-  owner: "owner",            // ← ton nom d'utilisateur/org GitHub
-  repo: "apartment-map",     // ← nom du repo
+  owner: "mathislef34",            // ← ton nom d'utilisateur/org GitHub
+  repo: "Appartements",     // ← nom du repo
   branch: "main",            // ← branche Pages (ex: main)
 };
+
